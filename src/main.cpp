a621ca7a54558cbff9f90c12e38e823c700066db
painnick <painnick@gmail.com>
2025-08-30 10:34:49 +0900

    UPDATE. README 및 코드 수정: 게임패드 조작 방식 변경 및 기관총 발사 기능 추가, LED 동작 및 효과음 관련 설명 업데이트.

--- src/main.cpp
+++ src/main.cpp
@@ -77,9 +77,15 @@ bool cannonFiring = false;
 unsigned long cannonStartTime = 0;
 const unsigned long cannonDuration = 500; // 500ms 동안 포신 당김
 
+// 기관총 발사 관련 변수
+bool machinegunFiring = false;
+unsigned long machinegunStartTime = 0;
+const unsigned long machinegunDuration = 3000; // 3초간 기관총 발사
+
 // 효과음 파일 번호
 #define SOUND_IDLE 1
 #define SOUND_CANNON 2
+#define SOUND_MACHINEGUN 3
 
 // 게임패드 연결 콜백
 void onConnectedController(ControllerPtr ctl) {
@@ -208,31 +214,19 @@ void saveSpeedSettings() {
 
 // 게임패드 처리 함수
 void processGamepad(ControllerPtr ctl) {
-    // 좌측 스틱으로 트랙 제어
-    int leftStickX = ctl->axisX();
+    // 좌측 스틱 Y축으로 좌측 트랙 제어, 우측 스틱 Y축으로 우측 트랙 제어
     int leftStickY = ctl->axisY();
+    int rightStickY = ctl->axisRY();
 
     // 데드존 설정
-    if (abs(leftStickX) < 50) leftStickX = 0;
     if (abs(leftStickY) < 50) leftStickY = 0;
+    if (abs(rightStickY) < 50) rightStickY = 0;
 
-    // 좌측 스틱 Y축으로 전진/후진 제어
+    // 좌측 스틱 Y축으로 좌측 트랙 전후진 제어
     int leftTrackSpeed = map(leftStickY, -512, 512, -255, 255);
-    int rightTrackSpeed = leftTrackSpeed;
-
-    // 좌측 스틱 X축으로 회전 제어
-    if (leftStickX != 0) {
-        int turnSpeed = map(abs(leftStickX), 0, 512, 0, 255);
-        if (leftStickX > 0) {
-            // 우회전
-            leftTrackSpeed += turnSpeed;
-            rightTrackSpeed -= turnSpeed;
-        } else {
-            // 좌회전
-            leftTrackSpeed -= turnSpeed;
-            rightTrackSpeed += turnSpeed;
-        }
-    }
+    
+    // 우측 스틱 Y축으로 우측 트랙 전후진 제어
+    int rightTrackSpeed = map(rightStickY, -512, 512, -255, 255);
 
     // 속도 제한
     leftTrackSpeed = constrain(leftTrackSpeed, -255, 255);
@@ -260,27 +254,27 @@ void processGamepad(ControllerPtr ctl) {
     setMotorSpeed(LEFT_TRACK_IN1, LEFT_TRACK_IN2, LEFT_TRACK_MCPWM_UNIT, LEFT_TRACK_MCPWM_TIMER, leftTrackSpeed, &prevLeftTrackSpeed);
     setMotorSpeed(RIGHT_TRACK_IN1, RIGHT_TRACK_IN2, RIGHT_TRACK_MCPWM_UNIT, RIGHT_TRACK_MCPWM_TIMER, rightTrackSpeed, &prevRightTrackSpeed);
 
-    // 우측 스틱으로 터렛과 포 마운트 제어
-    int rightStickX = ctl->axisRX();
-    int rightStickY = ctl->axisRY();
-
-    // 데드존 설정
-    if (abs(rightStickX) < 50) rightStickX = 0;
-    if (abs(rightStickY) < 50) rightStickY = 0;
-
-    // 우측 스틱 X축으로 터렛 제어
-    int turretSpeed = map(rightStickX, -512, 512, -255, 255);
+    // D-PAD로 터렛과 포 마운트 제어
+    // D-PAD 좌우로 터렛 제어
+    int turretSpeed = 0;
+    if (ctl->dpadLeft()) {
+        turretSpeed = -255; // 좌측 회전
+    } else if (ctl->dpadRight()) {
+        turretSpeed = 255;  // 우측 회전
+    }
     setMotorSpeed(TURRET_IN1, TURRET_IN2, TURRET_MCPWM_UNIT, TURRET_MCPWM_TIMER, turretSpeed, &prevTurretSpeed);
 
-    // 우측 스틱 Y축으로 포 마운트 각도 제어
-    if (rightStickY != 0) {
-        cannonMountAngle = map(rightStickY, -512, 512, 0, 180);
-        cannonMountAngle = constrain(cannonMountAngle, 0, 180);
+    // D-PAD 상하로 포 마운트 각도 제어
+    if (ctl->dpadUp()) {
+        cannonMountAngle = constrain(cannonMountAngle + 2, 0, 180); // 위로 올림
+        setCannonMountAngle(cannonMountAngle);
+    } else if (ctl->dpadDown()) {
+        cannonMountAngle = constrain(cannonMountAngle - 2, 0, 180); // 아래로 내림
         setCannonMountAngle(cannonMountAngle);
     }
 
     // A 버튼으로 포신 발사
-    if (ctl->a() && !cannonFiring) {
+    if (ctl->a() && !cannonFiring && !machinegunFiring) {
         cannonFiring = true;
         cannonStartTime = millis();
         ledBlinking = true;
@@ -293,6 +287,19 @@ void processGamepad(ControllerPtr ctl) {
 
         // 효과음 2 재생
         myDFPlayer.play(SOUND_CANNON);
+    }
+
+    // B 버튼으로 기관총 발사
+    if (ctl->b() && !machinegunFiring && !cannonFiring) {
+        machinegunFiring = true;
+        machinegunStartTime = millis();
+        ledBlinking = true;
+
+        // 게임 패드 진동
+        ctl->playDualRumble(0, 200, 0xFF , 0x0);
+
+        // 효과음 3 재생
+        myDFPlayer.play(SOUND_MACHINEGUN);
     }
 
     // X 버튼으로 헤드라이트 토글
@@ -349,14 +356,41 @@ void processCannonFiring() {
         if (currentTime - cannonStartTime >= cannonDuration) {
             // 포신 발사 완료
             cannonFiring = false;
-            ledBlinking = false;
-            digitalWrite(LED_PIN, LOW);
+            
+            // 기관총이 발사 중이 아닌 경우에만 LED 점멸 중단
+            if (!machinegunFiring) {
+                ledBlinking = false;
+                digitalWrite(LED_PIN, LOW);
+            }
 
             // 포신을 원래 각도로 복원
             setCannonAngle(cannonAngle);
 
             // 효과음 1 재생 재개 (게임패드가 연결되어 있지 않은 경우)
-            if (!gamepadConnected) {
+            if (!gamepadConnected && !machinegunFiring) {
+                myDFPlayer.play(SOUND_IDLE);
+                lastIdleSoundTime = millis();
+            }
+        }
+    }
+}
+
+// 기관총 발사 처리
+void processMachinegunFiring() {
+    if (machinegunFiring) {
+        unsigned long currentTime = millis();
+        if (currentTime - machinegunStartTime >= machinegunDuration) {
+            // 기관총 발사 완료
+            machinegunFiring = false;
+            
+            // 포신이 발사 중이 아닌 경우에만 LED 점멸 중단
+            if (!cannonFiring) {
+                ledBlinking = false;
+                digitalWrite(LED_PIN, LOW);
+            }
+
+            // 효과음 1 재생 재개 (게임패드가 연결되어 있지 않은 경우)
+            if (!gamepadConnected && !cannonFiring) {
                 myDFPlayer.play(SOUND_IDLE);
                 lastIdleSoundTime = millis();
             }
@@ -368,16 +402,26 @@ void processCannonFiring() {
 void processLEDBlinking() {
     if (ledBlinking) {
         unsigned long currentTime = millis();
-        if (currentTime - lastBlinkTime >= blinkInterval) {
-            digitalWrite(LED_PIN, !digitalRead(LED_PIN));
-            lastBlinkTime = currentTime;
+        
+        // 기관총 발사 중일 때는 500ms 간격으로 점멸
+        if (machinegunFiring) {
+            if (currentTime - lastBlinkTime >= 500) {
+                digitalWrite(LED_PIN, !digitalRead(LED_PIN));
+                lastBlinkTime = currentTime;
+            }
+        } else {
+            // 포신 발사 중일 때는 기존 100ms 간격으로 점멸
+            if (currentTime - lastBlinkTime >= blinkInterval) {
+                digitalWrite(LED_PIN, !digitalRead(LED_PIN));
+                lastBlinkTime = currentTime;
+            }
         }
     }
 }
 
 // 효과음 반복 재생 처리
 void processIdleSound() {
-    if (!gamepadConnected && !cannonFiring) {
+    if (!gamepadConnected && !cannonFiring && !machinegunFiring) {
         unsigned long currentTime = millis();
         if (currentTime - lastIdleSoundTime >= idleSoundInterval) {
             myDFPlayer.play(SOUND_IDLE);
@@ -399,10 +443,6 @@ void processControllers() {
 
 // 설정 함수
 void setup() {
-
-    // 전원 공급을 위한 대기
-    delay(2000);
-
     // Serial.begin(115200);
     ESP_LOGI(MAIN_TAG, "RC Tank Initialization...");
 
@@ -493,6 +533,9 @@ void loop() {
 
     // 포신 발사 처리
     processCannonFiring();
+
+    // 기관총 발사 처리
+    processMachinegunFiring();
 
     // LED 깜빡임 처리
     processLEDBlinking();
